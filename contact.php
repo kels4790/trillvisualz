e8f4c951a9f0434295ea2a822935689a6194f7f2
LeslieHarris <kels4790@gmail.com>
2022-07-01 22:11:15 -0400

    Add existing project files to Git

---
+++ contact.php
@@ -0,0 +1,6 @@
+if($_POST) {
+    Sclient_name = "";
+    Sclient_email = "";
+    $client_phone = "";
+    $client_feedback = "";
+}
--- scripts.js
+++ scripts.js
@@ -2,4 +2,7 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip('show');
   })
 
-  
+  $('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
